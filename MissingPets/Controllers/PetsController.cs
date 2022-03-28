374afc251e1dd8d8c4b1872ff8f9f6581575cd35
Agustin Ojeda <agustinojeda20@gmail.com>
2022-03-28 17:56:12 -0300

    Add Authorize

--- MissingPets/Controllers/PetsController.cs
+++ MissingPets/Controllers/PetsController.cs
@@ -8,11 +8,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MissingPets.Tools;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MissingPets.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class PetsController : ControllerBase
     {
         private readonly IRepository<Pet> petsRepository;
