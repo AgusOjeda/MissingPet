bf11d97ee550eb966e3901ea9385082d3ce42874
Agustin Ojeda <agustinojeda20@gmail.com>
2022-03-28 18:01:01 -0300

    Add Configuration for JWT

--- MissingPets/Startup.cs
+++ MissingPets/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,9 +7,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using MissingPets.Models;
 using MissingPets.MongoDB;
+using MissingPets.Services;
 using MissingPets.Settings;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
@@ -17,6 +20,7 @@ using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MissingPets
@@ -25,6 +29,7 @@ namespace MissingPets
     {
         private const string AllowedOriginSetting = "AllowedOrigin";
         private ServiceSettings _serviceSettings;
+        readonly string MiCors = "MiCors";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -38,6 +43,16 @@ namespace MissingPets
 
             _serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
+            services.AddCors(options => {
+                options.AddPolicy(name: MiCors,
+                                    builder =>
+                                    {
+                                        builder.WithMethods("*");
+                                        builder.WithHeaders("*");
+                                        builder.WithOrigins("*");
+                                    });
+            });
+
 
             services.AddMongo()
                 .AddMongoRepository<Pet>("pets")
@@ -47,13 +62,40 @@ namespace MissingPets
             {
                 options.SuppressAsyncSuffixInActionNames = false;
             });
+            //.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            // CONFIGURE JWT
+            var appSettings = Configuration.GetSection(nameof(AppSettings)).Get<AppSettings>();
+            var llave = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(llave),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
 
             services.AddControllers();
+            services.AddScoped<IUserService, UserService>();
+                
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MissingPets", Version = "v1" });
             });
-        }
+            }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -74,7 +116,7 @@ namespace MissingPets
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
